6365c48dff5ba183e8ade3f6915d0d9e1432d009
tamirboe <79935267+tamirboe@users.noreply.github.com>
2021-03-11 14:57:12 +0200

    targil2.py

---
+++ targil1.py
@@ -0,0 +1,4 @@
+hours=input("enter num of hours")
+rate=input("enter the hourly rate:")
+pay=float(hours)*float(rate)
+print("payment is:", pay)
---
+++ targil2.py
@@ -0,0 +1,4 @@
+x=input("insert a num")
+y=input("insert a num")
+z=int(x)+int(y)
+print("sum of x and y is:" z)
